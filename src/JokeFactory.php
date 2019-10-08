143f5214ee253ebfd691e49f20060826d8be6931
Macjoe Umanah <macjoeu5@gmail.com>
2019-10-08 15:47:42 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -4,16 +4,13 @@ namespace MacjoeUmanah\MacPak;
 
 class JokeFactory
 {
+    protected $jokes = [];
 
-	protected $jokes=[];
+    public function __construct(array $jokes = null)
+    {
+        $this->jokes = $jokes;
+    }
 
-	public function __construct(array $jokes=null){
-
-
-		$this->jokes=$jokes;
-
-
-	}
     public function getRandomJoke()
     {
         return $this->jokes[array_rand($this->jokes)];
