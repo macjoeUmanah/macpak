a81b8cbd6b9a1697f18f4a615fbb512194608ecd
Macjoe Umanah <macjoeu5@gmail.com>
2019-10-22 16:36:14 +0100

    Apply fixes from StyleCI (#3)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -1,21 +1,24 @@
 <?php
 
 namespace MacjoeUmanah\MacPak;
+
 use GuzzleHttp\Client;
 
 class JokeFactory
 {
-
-     const API_ENDPOINT = 'http://api.icndb.com/jokes/random';
+    const API_ENDPOINT = 'http://api.icndb.com/jokes/random';
     protected $client;
+
     public function __construct(Client $client = null)
     {
         $this->client = $client ?: new Client();
     }
+
     public function getRandomJoke()
     {
         $response = $this->client->get(self::API_ENDPOINT);
         $joke = json_decode($response->getBody()->getContents());
+
         return $joke->value->joke;
     }
 }
